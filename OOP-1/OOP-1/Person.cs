de88c6e6964f7286cdb07ed893ff649eb1ff6fc1
Alex Hamadey <128324646+ahamadey27@users.noreply.github.com>
2025-08-07 11:20:09 -0400

    Add project files and build artifacts for OOP-1
    - Generated OOP-1.dll and OOP-1.csproj.nuget.dgspec.json for project restoration.
    - Created OOP-1.csproj.nuget.g.props and OOP-1.csproj.nuget.g.targets for NuGet integration.
    - Added project.assets.json and project.nuget.cache for dependency management.
    - Configured target framework to net8.0 with necessary properties for package restoration.

---
+++ OOP-1/OOP-1/Person.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OOP_1
+{
+    public class Person
+    {
+        private string name;
+        private int age;
+
+        
+    }
+}
